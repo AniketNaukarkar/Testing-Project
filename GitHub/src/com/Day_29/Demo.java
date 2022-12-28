aff8575c20183168f4b18328043acae7c7787ba8
anike <anike@Aniket>
2022-12-28 16:12:26 +0530

    By Aniket

---
+++ GitHub/src/com/Day_29/Demo.java
@@ -0,0 +1,14 @@
+package com.Day_29;
+
+public class Demo {
+	public void m1()
+	{
+		System.out.println("Hello m1");
+	}
+	public static void main(String[] args) {
+		{
+			System.out.println("Hello github");
+		}
+	}
+
+}
